2e51251ce4918991a16fc703e68f0c6e676d41ac
Leonid Beder <leonid@orbs.com>
2018-10-10 15:45:14 +0300

    No need to add CanReclaimToken.sol to the unification script

--- scripts/unify.sh
+++ scripts/unify.sh
@@ -11,7 +11,6 @@ echo "pragma solidity 0.4.24;" > Unified.sol
 unify ../node_modules/zeppelin-solidity/contracts/math/Math.sol
 unify ../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol
 unify ../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol
-unify ../node_modules/zeppelin-solidity/contracts/ownership/CanReclaimToken.sol
 unify ../node_modules/zeppelin-solidity/contracts/ownership/HasNoContracts.sol
 unify ../node_modules/zeppelin-solidity/contracts/ownership/HasNoTokens.sol
 unify ../node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol
