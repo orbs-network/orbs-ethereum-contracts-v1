d457b096e71b5b9f6dba093d851bc29a51f911a7
Leonid Beder <leonid@orbs.com>
2018-10-11 16:35:34 +0300

    Rename zeppelin-solidity to openzeppelin-solidity

--- scripts/unify.sh
+++ scripts/unify.sh
@@ -8,14 +8,14 @@ function unify() {
 
 echo "pragma solidity 0.4.24;" > Unified.sol
 
-unify ../node_modules/zeppelin-solidity/contracts/math/Math.sol
-unify ../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol
-unify ../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol
-unify ../node_modules/zeppelin-solidity/contracts/ownership/HasNoContracts.sol
-unify ../node_modules/zeppelin-solidity/contracts/ownership/HasNoTokens.sol
-unify ../node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol
-unify ../node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20.sol
-unify ../node_modules/zeppelin-solidity/contracts/token/ERC20/SafeERC20.sol
+unify ../node_modules/openzeppelin-solidity/contracts/math/Math.sol
+unify ../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol
+unify ../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol
+unify ../node_modules/openzeppelin-solidity/contracts/ownership/HasNoContracts.sol
+unify ../node_modules/openzeppelin-solidity/contracts/ownership/HasNoTokens.sol
+unify ../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol
+unify ../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol
+unify ../node_modules/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol
 
 unify ../contracts/DateTime.sol
 unify ../contracts/SubscriptionBilling.sol
