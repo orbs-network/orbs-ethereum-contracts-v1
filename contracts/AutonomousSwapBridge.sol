b3114c85875141aae0fe947b08db85cf23d4958a
Leonid Beder <leonid@orbs.com>
2018-11-13 09:28:40 +0200

    Add transferOut flow

--- contracts/AutonomousSwapBridge.sol
+++ contracts/AutonomousSwapBridge.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.4.24;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 import "./IFederation.sol";
@@ -7,6 +8,8 @@ import "./IFederation.sol";
 
 /// @title Autonomous Swap Bridge (ASB) smart contract.
 contract AutonomousSwapBridge {
+    using SafeMath for uint256;
+
     // The version of the current ASB smart contract.
     string public constant VERSION = "0.1";
 
@@ -22,6 +25,11 @@ contract AutonomousSwapBridge {
     // The federation smart contract.
     IFederation public federation;
 
+    // Incremental counter for Transaction Unique Identifiers (TUID).
+    uint256 public tuidCounter = 0;
+
+    event TransferredOut(address indexed from, bytes20 indexed to, uint256 value, uint256 tuid);
+
     /// @dev Constructor that initializes the ASB contract.
     /// @param _virtualChainId uint32 The virtual chain ID of the underlying token on the Orbs network.
     /// @param _orbsASBContractName string The address of the Federation contract.
@@ -35,5 +43,34 @@ contract AutonomousSwapBridge {
         orbsASBContractName = _orbsASBContractName;
         token = _token;
         federation = _federation;
+    }
+
+    /// @dev Transfer tokens to Orbs. The method retrieves and locks the tokens and emits the TransferredOut event.
+    /// @param _to string The Orbs address to transfer to.
+    /// @param _value uint256 The amount to be transferred.
+    function transferOut(bytes20 _to, uint256 _value) public {
+        require(isOrbsAddressValid(_to), "Orbs address is invalid!");
+        require(_value > 0, "Value must be greater than 0!");
+
+        // Verify that the requested approved enough tokens to transfer out.
+        require(token.transferFrom(msg.sender, address(this), _value), "Insufficient allowance!");
+
+        // Get a fresh TUID. Please note that a uint256 overflow isn't a concern here, since it can only happen after
+        // 2^256 transactions (in which case, it will be no longer possible to issue transfers using this specific
+        // instance of the ASB smart contract).
+        tuidCounter = tuidCounter.add(1);
+
+        emit TransferredOut(msg.sender, _to, _value, tuidCounter);
+    }
+
+    /// @dev Checks Orbs address for correctness.
+    /// @param _address bytes20 The Orbs address to check.
+    function isOrbsAddressValid(bytes20 _address) private pure returns (bool) {
+        // Check for empty address.
+        if (_address == bytes20(0)) {
+            return false;
+        }
+
+        return true;
     }
 }
--- test/AutonomousSwapBridge.js
+++ test/AutonomousSwapBridge.js
@@ -62,4 +62,125 @@ contract('AutonomousSwapBridge', (accounts) => {
       expect(await asb.VERSION.call()).to.be.bignumber.equal(VERSION);
     });
   });
+
+  describe('transfer tokens to Orbs', async () => {
+    let asb;
+    const initialBalance = 100000;
+    const user1 = accounts[3];
+    const user2 = accounts[4];
+    const user3 = accounts[5];
+    const orbsUser1Address = '0x1fad2e43de9d6b4d8b0711f499b7b1b445170b6a';
+    const orbsUser2Address = '0x02786db0e65e76bd8043031f6a6292cbc763d010';
+    const orbsUser3Address = '0x99a8487019099bee8af8473134fa247c2f018790';
+
+    beforeEach(async () => {
+      asb = await AutonomousSwapBridge.new(VIRTUAL_CHAIN_ID, ORBS_ASB_CONTRACT_NAME, token.address,
+        federation.address, { from: owner });
+
+      await token.assign(user1, initialBalance);
+      await token.assign(user2, initialBalance);
+      await token.assign(user3, initialBalance);
+    });
+
+    it('should transfer out tokens', async () => {
+      const value = 100;
+      await token.approve(asb.address, value, { from: user1 });
+
+      const asbBalance = await token.balanceOf(asb.address);
+      const tx = await asb.transferOut(orbsUser1Address, value, { from: user1 });
+
+      expect(tx.logs).to.have.length(1);
+      const event = tx.logs[0];
+      expect(event.event).to.eql('TransferredOut');
+      expect(event.args.from).to.eql(user1);
+      expect(event.args.to).to.eql(orbsUser1Address);
+      expect(event.args.value).to.be.bignumber.equal(value);
+      expect(event.args.tuid).to.be.bignumber.equal(1);
+
+      expect(await token.balanceOf(asb.address)).to.be.bignumber.equal(asbBalance.plus(value));
+    });
+
+    it('should transfer out tokens more than once', async () => {
+      const value = 1000;
+      await token.approve(asb.address, value, { from: user1 });
+
+      const times = 20;
+      for (let i = 0; i < times; ++i) {
+        const asbBalance = await token.balanceOf(asb.address);
+        const tx = await asb.transferOut(orbsUser1Address, value / times, { from: user1 });
+        expect(tx.logs).to.have.length(1);
+        const event = tx.logs[0];
+        expect(event.event).to.eql('TransferredOut');
+        expect(event.args.from).to.eql(user1);
+        expect(event.args.to).to.eql(orbsUser1Address);
+        expect(event.args.value).to.be.bignumber.equal(value / times);
+        expect(event.args.tuid).to.be.bignumber.equal(i + 1);
+
+        expect(await token.balanceOf(asb.address)).to.be.bignumber.equal(asbBalance.plus(value / times));
+      }
+    });
+
+    it('should transfer out tokens from multiple token holders', async () => {
+      const value = 10000;
+      [user1, user2, user3].forEach(async (user) => {
+        await token.approve(asb.address, value, { from: user });
+      });
+
+      const scenarios = [
+        { from: user1, to: orbsUser1Address, value: 200 },
+        { from: user2, to: orbsUser2Address, value: 30 },
+        { from: user2, to: orbsUser3Address, value: 570 },
+        { from: user3, to: orbsUser1Address, value: 5 },
+        { from: user3, to: orbsUser2Address, value: 58 },
+        { from: user1, to: orbsUser3Address, value: 111 },
+        { from: user2, to: orbsUser1Address, value: 899 },
+        { from: user3, to: orbsUser2Address, value: 1000 },
+      ];
+
+      for (let i = 0; i < scenarios.length; ++i) {
+        const spec = scenarios[i];
+
+        const asbBalance = await token.balanceOf(asb.address);
+
+        const tx = await asb.transferOut(spec.to, spec.value, { from: spec.from });
+        expect(tx.logs).to.have.length(1);
+        const event = tx.logs[0];
+        expect(event.event).to.eql('TransferredOut');
+        expect(event.args.from).to.eql(spec.from);
+        expect(event.args.to).to.eql(spec.to);
+        expect(event.args.value).to.be.bignumber.equal(spec.value);
+        expect(event.args.tuid).to.be.bignumber.equal(i + 1);
+
+        expect(await token.balanceOf(asb.address)).to.be.bignumber.equal(asbBalance.plus(spec.value));
+      }
+    });
+
+    it('should not allow to transfer out more than allowed tokens', async () => {
+      const value = 100;
+      await token.approve(asb.address, value, { from: user1 });
+
+      await expectRevert(asb.transferOut(orbsUser1Address, value + 1, { from: user1 }));
+    });
+
+    it('should not allow to transfer out more than the tokens that the user holds', async () => {
+      const value = initialBalance + 1000;
+      await token.approve(asb.address, value, { from: user1 });
+
+      await expectRevert(asb.transferOut(orbsUser1Address, value, { from: user1 }));
+    });
+
+    it('should not allow to transfer to an empty Orbs address', async () => {
+      const value = 100;
+      await token.approve(asb.address, value, { from: user1 });
+
+      await expectRevert(asb.transferOut(EMPTY, value, { from: user1 }));
+    });
+
+    it('should not allow to transfer 0 tokens', async () => {
+      const value = 100;
+      await token.approve(asb.address, value, { from: user1 });
+
+      await expectRevert(asb.transferOut(orbsUser1Address, 0, { from: user1 }));
+    });
+  });
 });
