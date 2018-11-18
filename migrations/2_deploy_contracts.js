7204f9fa2aef899166e402564a2641c1f2053377
Leonid Beder <leonid@orbs.com>
2018-11-18 18:35:49 +0200

    Properly sync migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,12 @@
 const DateTime = artifacts.require('./DateTime.sol');
+const AutonomousSwapBridge = artifacts.require('./AutonomousSwapBridge.sol');
+const AutonomousSwapProofVerifier = artifacts.require('./AutonomousSwapProofVerifier.sol');
+
 const DateTimeWrapper = artifacts.require('../test/helpers/DateTimeWrapper.sol');
 const SubscriptionManager = artifacts.require('../test/SubscriptionManager.sol');
 const SubscriptionManagerMock = artifacts.require('../test/SubscriptionManagerMock.sol');
 
-module.exports = async (deployer, network) => {
+async function deploy(deployer, network) {
   // We're only using these migrations during development and testing.
   if (network !== 'development' && network !== 'coverage') {
     return;
@@ -13,4 +16,14 @@ module.exports = async (deployer, network) => {
   await deployer.link(DateTime, DateTimeWrapper);
   await deployer.link(DateTime, SubscriptionManager);
   await deployer.link(DateTime, SubscriptionManagerMock);
+
+
+  await deployer.deploy(AutonomousSwapProofVerifier);
+  await deployer.link(AutonomousSwapProofVerifier, AutonomousSwapBridge);
+}
+
+module.exports = (deployer, network) => {
+  deployer.then(async () => {
+    await deploy(deployer, network);
+  });
 };
