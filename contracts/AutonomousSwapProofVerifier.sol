91e4b8a869a3d33477c0179366a46ac44c709674
Leonid Beder <leonid@orbs.com>
2018-12-06 21:42:59 +0200

    Add parseResultsBlockProof (WIP)

--- contracts/AutonomousSwapProofVerifier.sol
+++ contracts/AutonomousSwapProofVerifier.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.4.24;
+pragma experimental ABIEncoderV2;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
@@ -23,7 +24,19 @@ contract AutonomousSwapProofVerifier is IAutonomousSwapProofVerifier {
     uint public constant UINT64_SIZE = 8;
     uint public constant UINT256_SIZE = 32;
     uint public constant ADDRESS_SIZE = 20;
-    uint public constant BYTES32_SIZE = UINT256_SIZE;
+    uint public constant SHA256_SIZE = UINT256_SIZE;
+    uint public constant SIGNATURE_SIZE = 65;
+
+    // Orbs specific data sizes (in bytes).
+    uint public constant ONEOF_NESTING_SIZE = 12;
+    uint public constant BLOCKREFMESSAGE_SIZE = 52;
+    uint public constant BLOCKHASH_OFFSET = 20;
+    uint public constant NODE_PK_SIG_NESTING_SIZE = 4;
+
+    // The maximum supported number of signatures in Results Block Proof. We have to limit this number and fallback to
+    // statically sized lists, due to Solidity's inability of functions returning dynamic arrays (and limiting gas
+    // consumption, of course).
+    uint public constant MAX_SIGNATURES = 32;
 
     /// @dev Parses and validates the raw transfer proof.
     /// @param _proof bytes The raw transfer proof.
@@ -74,6 +87,72 @@ contract AutonomousSwapProofVerifier is IAutonomousSwapProofVerifier {
 
         receiptMerkleRoot = _resultsBlockHeader.toBytes32(offset);
         offset = offset.add(SHA256_SIZE);
+    }
+
+    /// @dev Parses Results Block Proof according to:
+    /// +--------------------------------+------------+-----------+-------------+--------------------------+
+    /// |             Field              |   Offset   |   Size    |  Encoding   |          Notes           |
+    /// +--------------------------------+------------+-----------+-------------+--------------------------+
+    /// | block_proof_version            | 0          | 4         | uint32      |                          |
+    /// | transactions_block_hash length | 4          | always 4  | reserved    |                          |
+    /// | transactions_block_hash        | 8          | 32        | bytes (32B) |                          |
+    /// | oneof + nesting                | 40         | 12        | reserved    | oneof + proof + blockref |
+    /// | blockref_message               | 52         | 52        | bytes (52B) |                          |
+    /// | block_hash                     | 72         | 32        | bytes (32B) |                          |
+    /// | node_pk_sig nesting            | 104 + 100n | reserved  |             |                          |
+    /// | node_pk_length                 | 108 + 100n | 4         | always 20   | reserved                 |
+    /// | node_pk                        | 112 + 100n | 20        | bytes (20B) | Ethereum address         |
+    /// | node_sig_length                | 132 + 100n | 4         | always 65   | reserved                 |
+    /// | node_sig                       | 136 + 100n | 65        | bytes (65B) |                          |
+    /// +--------------------------------+------------+-----------+-------------+--------------------------+
+    /// @param _resultsBlockProof bytes The serialized data.
+    /// @return blockProofVersion uint32 The version of the block proof (== the federation revision).
+    /// @return transactionsBlockHash bytes32 The hash of the transactions block.
+    /// @return blockrefMessage bytes The block reference message.
+    /// @return blockHash bytes32 The hash of the block.
+    /// @return numOfSignatures uint The number of signatures in the Results Block Proof.
+    /// @return publicAddresses address[] The public addresses of the signing federation members.
+    /// @return signatures bytes[] The respective signatures by the signing federation members.
+    function parseResultsBlockProof(bytes _resultsBlockProof) public pure returns (uint32 blockProofVersion,
+        bytes32 transactionsBlockHash, bytes blockrefMessage, bytes32 blockHash, uint numOfSignatures,
+        address[MAX_SIGNATURES] publicAddresses, bytes[MAX_SIGNATURES] signatures) {
+        uint offset = 0;
+
+        blockProofVersion = _resultsBlockProof.toUint32BE(offset);
+        offset = offset.add(UINT32_SIZE);
+
+        uint32 transactionsBlockHashSize =_resultsBlockProof.toUint32BE(offset);
+        require(transactionsBlockHashSize == SHA256_SIZE, "Invalid hash size!");
+        offset = offset.add(UINT32_SIZE);
+        transactionsBlockHash = _resultsBlockProof.toBytes32(offset);
+        offset = offset.add(SHA256_SIZE);
+
+        offset = offset.add(ONEOF_NESTING_SIZE); // oneof + nesting
+        blockrefMessage = _resultsBlockProof.slice(offset, BLOCKREFMESSAGE_SIZE);
+        offset = offset.add(BLOCKHASH_OFFSET);
+
+        blockHash = _resultsBlockProof.toBytes32(offset);
+        offset = offset.add(SHA256_SIZE);
+
+        // Note: in the case that the remaining buffer is too small - we will either revert in SafeMath or in
+        // BytesUtils/Ex.
+        while (offset < _resultsBlockProof.length) {
+            offset = offset.add(NODE_PK_SIG_NESTING_SIZE); // node_pk_sig nesting
+
+            uint32 publicAddressSize =_resultsBlockProof.toUint32BE(offset);
+            require(publicAddressSize == ADDRESS_SIZE, "Invalid address size!");
+            offset = offset.add(UINT32_SIZE);
+            publicAddresses[numOfSignatures] = _resultsBlockProof.toAddress(offset);
+            offset = offset.add(ADDRESS_SIZE);
+
+            uint32 signatureSize =_resultsBlockProof.toUint32BE(offset);
+            require(signatureSize == SIGNATURE_SIZE, "Invalid signature size!");
+            offset = offset.add(UINT32_SIZE);
+            signatures[numOfSignatures] = _resultsBlockProof.slice(offset, SIGNATURE_SIZE);
+            offset = offset.add(SIGNATURE_SIZE);
+
+            numOfSignatures = numOfSignatures.add(1);
+        }
     }
 
     /// @dev Parses Autonomous Swap Event Data according to:
--- test/helpers/asbProof.js
+++ test/helpers/asbProof.js
@@ -4,6 +4,8 @@ const UINT32_SIZE = 4;
 const UINT64_SIZE = 8;
 const UINT256_SIZE = 32;
 const ADDRESS_SIZE = 20;
+const SHA256_SIZE = UINT256_SIZE;
+const SIGNATURE_SIZE = 65;
 
 class ASBProof {
   // Builds the Results Block Header according to:
@@ -26,6 +28,45 @@ class ASBProof {
       resultsBlockHeader.receiptMerkleRoot,
     ]);
   }
+
+  // Builds the Results Block Proof according to:
+  // +--------------------------------+------------+-----------+-------------+--------------------------+
+  // |             Field              |   Offset   |   Size    |  Encoding   |          Notes           |
+  // +--------------------------------+------------+-----------+-------------+--------------------------+
+  // | block_proof_version            | 0          | 4         | uint32      |                          |
+  // | transactions_block_hash length | 4          | always 4  | reserved    |                          |
+  // | transactions_block_hash        | 8          | 32        | bytes (32B) |                          |
+  // | oneof + nesting                | 40         | 12        | reserved    | oneof + proof + blockref |
+  // | blockref_message               | 52         | 52        | bytes (52B) |                          |
+  // | block_hash                     | 72         | 32        | bytes (32B) |                          |
+  // | node_pk_sig nesting            | 104 + 100n | reserved  |             |                          |
+  // | node_pk_length                 | 108 + 100n | 4         | always 20   | reserved                 |
+  // | node_pk                        | 112 + 100n | 20        | bytes (20B) | Ethereum address         |
+  // | node_sig_length                | 132 + 100n | 4         | always 65   | reserved                 |
+  // | node_sig                       | 136 + 100n | 65        | bytes (65B) |                          |
+  // +--------------------------------+------------+-----------+-------------+--------------------------+
+  static buildResultsProof(resultsBlockProof) {
+    let resultsBlockProofBuffer = Buffer.concat([
+      Bytes.numberToBuffer(resultsBlockProof.blockProofVersion, 4),
+      Bytes.numberToBuffer(SHA256_SIZE, 4),
+      resultsBlockProof.transactionsBlockHash,
+      Buffer.alloc(12), // one-of + nesting
+      resultsBlockProof.blockrefMessage,
+    ]);
+
+    resultsBlockProof.signatures.forEach((sig) => {
+      resultsBlockProofBuffer = Buffer.concat([resultsBlockProofBuffer,
+        Buffer.alloc(4), // node_pk_sig nesting
+        Bytes.numberToBuffer(ADDRESS_SIZE, 4),
+        Bytes.addressToBuffer(sig.publicAddress),
+        Bytes.numberToBuffer(SIGNATURE_SIZE, 4),
+        Bytes.addressToBuffer(sig.signature),
+      ]);
+    });
+
+    return resultsBlockProofBuffer;
+  }
+
   // Builds the Autonomous Swap Event Data according to:
   // +--------------------------+--------+------+-------------+-------------------------------+
   // |          Field           | Offset | Size |  Encoding   |             Notes             |
