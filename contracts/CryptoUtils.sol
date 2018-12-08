227c5efe69e033ca0af3af2c4840061fab364f2a
Leonid Beder <leonid@orbs.com>
2018-12-08 12:14:18 +0200

    Support passing of hash as well

--- contracts/CryptoUtils.sol
+++ contracts/CryptoUtils.sol
@@ -36,8 +36,18 @@ library CryptoUtils {
     /// construction).
     /// @param _proof bytes32[] The Merkle proof containing sibling hashes on the branch from the leaf to the root.
     /// @param _root bytes32 The Merkle root.
-    /// @param _leaf bytes.
+    /// @param _leaf bytes The data to check..
     function isMerkleProofValid(bytes32[] _proof, bytes32 _root, bytes _leaf) public pure returns (bool) {
-        return MerkleProof.verify(_proof, _root, keccak256(_leaf));
+        return isMerkleProofValid(_proof, _root, keccak256(_leaf));
+    }
+
+    /// @dev Verifies the Merkle proof for the existence of a specific data. Please not that that this implementation
+    /// assumes that each pair of leaves and each pair of pre-images are sorted (see tests for examples of
+    /// construction).
+    /// @param _proof bytes32[] The Merkle proof containing sibling hashes on the branch from the leaf to the root.
+    /// @param _root bytes32 The Merkle root.
+    /// @param _leafHash bytes32 The hash of the data to check..
+    function isMerkleProofValid(bytes32[] _proof, bytes32 _root, bytes32 _leafHash) public pure returns (bool) {
+        return MerkleProof.verify(_proof, _root, _leafHash);
     }
 }
