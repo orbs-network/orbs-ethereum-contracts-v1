81941120aa0e8cbf5ee2b871b6adfa36d5f2718b
Leonid Beder <leonid@orbs.com>
2018-12-09 13:09:08 +0200

    Add StringUtils

---
+++ contracts/StringUtils.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.4.24;
+
+
+/// @title String utilities.
+library StringUtils {
+    /// @dev Performs an efficient string comparison (assuming keccak256 collision resistance).
+    /// @param str1 The first string to compare.
+    /// @param str2 The second string to compare.
+    function equal(string memory str1, string memory str2) internal pure returns (bool) {
+        if (bytes(str1).length != bytes(str2).length) {
+            return false;
+        }
+
+
+        return keccak256(bytes(str1)) == keccak256(bytes(str2));
+    }
+}
