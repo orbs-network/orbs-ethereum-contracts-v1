58f2339fa6eec3d81095abc51cfa718b457f1e15
Noam Berg <noamberg@yahoo.com>
2018-12-23 17:07:43 +0200

    fix up the scripts to have try catch and call the done().

--- asb/getBalance.js
+++ asb/getBalance.js
@@ -1,17 +1,18 @@
+module.exports = async function(done) {
+  try {
+    const Tet = artifacts.require('Tet.sol');
+    let tet = await Tet.deployed();
+    if (process.argv.length != 5) {
+      console.error("Wrong number of arguments expect (truffle exec getBalance.js userAddr)\n");
+      exit(-1);
+    }
+    let userAddr = process.argv[4];
 
-const doSomthing = async () => {
-  const Tet = artifacts.require('Tet.sol');
-  let tet = await Tet.deployed();
-  if (process.argv.length != 5) {
-    console.error("Wrong number of arguments expect (truffle exec getBalance.js userAddr\n");
-    exit(-1);
+    let balance = await tet.balanceOf(userAddr, {from: userAddr})
+    console.log(`${balance}\n`)
+    done();
+  } catch (e) {
+    console.log(e);
+    done(e);
   }
-  let userAddr = process.argv[4];
-
-  let balance = await tet.balanceOf(userAddr, {from: userAddr})
-  console.log(`${balance}\n`)
-}
-
-module.exports = function(callback) {
-  doSomthing();
 }
--- asb/getasbaddr.js
+++ asb/getasbaddr.js
@@ -1,10 +1,11 @@
-
-const doSomthing = async () => {
-  const AutonomousSwapBridge = artifacts.require('AutonomousSwapBridge.sol');
-  let asbInstance = await AutonomousSwapBridge.deployed();
-  console.log(`${asbInstance.address} \n`)
-}
-
-module.exports = function(callback) {
-  doSomthing();
+module.exports = async function(done) {
+  try {
+    const AutonomousSwapBridge = artifacts.require('AutonomousSwapBridge.sol');
+    let asbInstance = await AutonomousSwapBridge.deployed();
+    console.log(`${asbInstance.address} \n`)
+    done();
+  } catch(e) {
+    console.log(e);
+    done(e);
+  }
 }
