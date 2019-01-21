8ab1f04a727627daac1bc9008d990fa5904d655d
itamar Arjuan <itamar.arjuan@gmail.com>
2019-01-21 13:02:40 +0200

    trying to trick gamma server container into thinking host.docker.internal answers as ganache

--- .circleci/main.sh
+++ .circleci/main.sh
@@ -6,6 +6,7 @@ docker build -t orbs:asb -f docker/images/asb/Dockerfile .
 echo "Starting up test environment.."
 docker-compose -f docker/compose/asb/docker-compose.yml down -v
 docker-compose -f docker/compose/asb/docker-compose.yml up -d
+docker exec asb_gamma_1 echo "ganache host.docker.internal" > /etc/hosts
 
 echo "Sleeping for a few seconds to let Ganache and Gamma become ready to accept connections"
 sleep 5
