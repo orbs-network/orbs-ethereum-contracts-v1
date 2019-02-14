98daf04f8832b95da18e985314409cd742af39a8
Ron <ron@orbs.com>
2019-02-14 03:52:11 +0200

    tests cleanup

--- votingContract/test/delegate_emits_event.js
+++ votingContract/test/delegate_emits_event.js
@@ -4,21 +4,20 @@ describe('Voting Contract', async function() {
     const activist = "0x0000000000000000000000000000000000000001";
     const instance = await VotingContract.deployed();
     const accounts = await web3.eth.getAccounts();
-    describe('delegate',  function() {
-        it('should emit event', async function() {
 
-            let receipt = await instance.delegate(activist);
+    describe('when calling the delegate() function',  function() {
+        it('should emit one Delegate event', async function() {
 
-            assert.equal(receipt.logs[0].event, "Delegate");
-            assert.equal(receipt.logs[0].args.stakeholder, accounts[0]);
-            assert.equal(receipt.logs[0].args.activist, activist);
-            assert.equal(receipt.logs[0].args.delegation_counter, 1);
+            const receipt = await instance.delegate(activist);
+            const e = receipt.logs[0];
+
+            assert.equal(e.event, "Delegate");
+            assert.equal(e.args.stakeholder, accounts[0]);
+            assert.equal(e.args.activist, activist);
+            assert.equal(e.args.delegation_counter, 1);
         });
 
-
-        it('should increment counter', async function() {
-            const VotingContract = artifacts.require('Voting');
-            let instance = await VotingContract.deployed();
+        it('should increment delegation_counter', async function() {
 
             let counter1 = await instance.delegate(activist).then(receipt => receipt.logs[0].args.delegation_counter.toNumber());
             let counter2 = await instance.delegate(activist).then(receipt => receipt.logs[0].args.delegation_counter.toNumber());
--- votingContract/test/vote_emits_event.js
+++ votingContract/test/vote_emits_event.js
@@ -1,34 +1,29 @@
 const assert = require('assert');
-describe('Voting Contract', async function() {
+describe('Voting Contract', function() {
+    const VotingContract = artifacts.require('Voting');
 
-    describe('vote', function() {
-        it('should emit event', async function() {
-            const VotingContract = artifacts.require('Voting');
+    describe('when calling the vote() function', function() {
+        it('should emit one Vote event', async function() {
             const instance = await VotingContract.deployed();
             const accounts = await web3.eth.getAccounts();
 
+            const receipt = await instance.vote(accounts);
+            const e = receipt.logs[0];
 
-            let receipt = await instance.vote(accounts);
-
-            assert.equal(receipt.logs[0].event, "Vote")
-            assert.equal(receipt.logs[0].args.activist, accounts[0]);
-            assert.equal(receipt.logs[0].args.vote_counter, 1)
-
-            assert.equal(receipt.logs[0].args.candidates.length, accounts.length);
-            receipt.logs[0].args.candidates.forEach((candidate, i) => {
-                assert.equal(candidate, accounts[i])
-            })
+            assert.equal(e.event, "Vote");
+            assert.equal(e.args.activist, accounts[0]);
+            assert.equal(e.args.vote_counter, 1);
+            assert.equal(JSON.stringify(e.args.candidates), JSON.stringify(accounts));
         });
 
-        it('should increment counter', async function() {
-            const VotingContract = artifacts.require('Voting');
+        it('should increment vote_counter', async function() {
             const instance = await VotingContract.deployed();
             const accounts = await web3.eth.getAccounts();
 
-            let voteCounter1 = await instance.vote(accounts).then(receipt => receipt.logs[0].args.vote_counter.toNumber());
-            let voteCounter2 = await instance.vote(accounts).then(receipt => receipt.logs[0].args.vote_counter.toNumber());
+            let counter1 = await instance.vote(accounts).then(receipt => receipt.logs[0].args.vote_counter.toNumber());
+            let counter2 = await instance.vote(accounts).then(receipt => receipt.logs[0].args.vote_counter.toNumber());
 
-            assert.equal(voteCounter2, voteCounter1 + 1)
+            assert.equal(counter2, counter1 + 1)
 
         });
     });
