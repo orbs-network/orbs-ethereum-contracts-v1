0dd050383d33124371779320659fb3722cab4c17
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-02-25 11:05:23 +0200

    client-web: move to create-react-app

--- Voting/client-web/scripts/deploy.sh
+++ Voting/client-web/scripts/deploy.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-npm run build
-npm run export
 git clone https://github.com/orbs-network/voting
 cd voting/
 git rm -rf *
-mv ../out/* ./
-touch .nojekyll
+cp -R ../build/* ./
 git add .
 git commit -m "deploy"
 git push origin master
