7de6c2aad7cd194eb5943ef806b842f70fe0eaab
Shai Yallin <shai.yallin@gmail.com>
2019-03-06 18:06:36 +0200

    ASB is now finality compliant

--- .circleci/main.sh
+++ .circleci/main.sh
@@ -5,6 +5,8 @@ docker build -t orbs:asb -f docker/images/asb/Dockerfile .
 
 echo "Starting up test environment.."
 docker-compose -f docker/compose/asb/docker-compose.yml down -v
+
+export GANACHE_START_TIME=$(node -e "console.log(new Date(new Date() - 120000))")
 docker-compose -f docker/compose/asb/docker-compose.yml up -d
 docker exec asb_gamma_1 echo "ganache host.docker.internal" > /etc/hosts
 
--- ASB/truffle-scripts/makeFinal.js
+++ ASB/truffle-scripts/makeFinal.js
@@ -1,41 +1,37 @@
-function moveTimeBy(seconds, callback) {
-    web3.currentProvider.send({
+const util = require('util');
+const sendRpc = util.promisify(web3.currentProvider.send);
+
+async function moveTimeBy(seconds) {
+    return sendRpc({
         jsonrpc: '2.0',
         method: 'evm_increaseTime',
         params: [seconds],
         id: new Date().getSeconds()
-    }, callback)
+    });
 }
 
-function mine(blocks, callback) {
-    web3.currentProvider.send({
+async function mine() {
+    return sendRpc({
         jsonrpc: '2.0',
         method: 'evm_mine',
         params: [],
         id: new Date().getSeconds()
-    }, err => {
-        if (err) {
-            console.log(err);
-            callback(err);
-        } else if (blocks === 0) {
-            callback()
-        } else {
-            mine(blocks - 1, callback)
-        }
-    })
+    });
 }
 
 module.exports = async function(done) {
+    console.log("Moving Ganache clock to current time by mining a block every 10 seconds")
     try {
-        moveTimeBy(90, err => {
-            if (!err) {
-                mine(10, done);
-            } else {
-                console.log(err);
-                done(err);
+        const now = new Date().getTime() / 1000;
+        let latestBlockTime = 0;
+        while (latestBlockTime < now) {
+            await moveTimeBy(10);
+            await mine();
 
-            }
-        });
+            let latestBlock = await web3.eth.getBlock("latest");
+            latestBlockTime = latestBlock.timestamp;
+            console.log("Mined a block at time", new Date(latestBlockTime * 1000));
+        }
 
     } catch (e) {
         console.log(e);
