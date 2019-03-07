51bdb240e59ea899e2c94bf6367d3fed8d1845cd
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-03-07 09:19:24 +0200

    client-web: home redesign

--- voting/client-web/src/components/Home/index.tsx
+++ voting/client-web/src/components/Home/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import { Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
@@ -8,32 +7,52 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   mainText: {
     paddingTop: theme.spacing.unit * 10
+  },
+  container: {
+    display: 'flex',
+    width: '100%',
+    height: '80vh',
+    flexDirection: 'column' as any
+  },
+  header: {
+    textAlign: 'center' as any,
+    paddingBottom: 20
+  },
+  columns: {
+    display: 'flex',
+    height: '100%'
+  },
+  column: {
+    flex: 1,
+    padding: 20,
+    textAlign: 'center' as any
+  },
+  columnText: {
+    height: '90%',
+    width: '90%',
+    margin: '0 auto'
   }
 });
 
 const Home = ({ classes }) => {
   return (
-    <Grid container justify="center">
-      <Grid item>
+    <div className={classes.container}>
+      <header className={classes.header}>
         <Typography variant="h3" color="textPrimary" noWrap>
           Who are you?
         </Typography>
-      </Grid>
-      <Grid
-        container
-        spacing={16}
-        justify="center"
-        direction="row"
-        className={classes.mainText}
-      >
-        <Grid item xs={4}>
-          <Typography variant="body1">
-            Delegators are token holders who assign responsibility for their
-            voting stake to Guardians, empowering them to align the network with
-            the best interests of the ecosystem. Delegators may also proxy their
-            voting stake to another Delegator trusting her to choose the right
-            guardian.
-          </Typography>
+      </header>
+      <section className={classes.columns}>
+        <article className={classes.column}>
+          <div className={classes.columnText}>
+            <Typography variant="body1">
+              Delegators are token holders who assign responsibility for their
+              voting stake to Guardians, empowering them to align the network
+              with the best interests of the ecosystem. Delegators may also
+              proxy their voting stake to another Delegator trusting her to
+              choose the right guardian.
+            </Typography>
+          </div>
           <Button variant="outlined" color="secondary">
             <Link to="/delegator">
               <Typography variant="subtitle1" color="textSecondary">
@@ -41,15 +60,17 @@ const Home = ({ classes }) => {
               </Typography>
             </Link>
           </Button>
-        </Grid>
-        <Grid item xs={4}>
-          <Typography variant="body1">
-            Guardians are expected to be key players and stakeholders within the
-            Universe who align with the long term Orbs vision and play a role in
-            making this vision a reality. A core role of the Guardians is to
-            review the validators, monitor their operations and approve and
-            elect the ideal actors.
-          </Typography>
+        </article>
+        <article className={classes.column}>
+          <div className={classes.columnText}>
+            <Typography variant="body1">
+              Guardians are expected to be key players and stakeholders within
+              the Universe who align with the long term Orbs vision and play a
+              role in making this vision a reality. A core role of the Guardians
+              is to review the validators, monitor their operations and approve
+              and elect the ideal actors.
+            </Typography>
+          </div>
           <Button variant="outlined" color="secondary">
             <Link to="/guardian">
               <Typography variant="subtitle1" color="textSecondary">
@@ -57,16 +78,18 @@ const Home = ({ classes }) => {
               </Typography>
             </Link>
           </Button>
-        </Grid>
-        <Grid item xs={4}>
-          <Typography variant="body1">
-            Validators operate the nodes of the Orbs network. They maintain the
-            block history and participate in block creation and validation.
-            Validators are required to maintain availability and connectivity
-            and provide the required quality of service. Validators are expected
-            to be technically capable and contribute to and support the
-            network’s development.
-          </Typography>
+        </article>
+        <article className={classes.column}>
+          <div className={classes.columnText}>
+            <Typography variant="body1">
+              Validators operate the nodes of the Orbs network. They maintain
+              the block history and participate in block creation and
+              validation. Validators are required to maintain availability and
+              connectivity and provide the required quality of service.
+              Validators are expected to be technically capable and contribute
+              to and support the network’s development.
+            </Typography>
+          </div>
           <Button variant="outlined" color="secondary">
             <Link to="/">
               <Typography variant="subtitle1" color="textSecondary">
@@ -74,9 +97,9 @@ const Home = ({ classes }) => {
               </Typography>
             </Link>
           </Button>
-        </Grid>
-      </Grid>
-    </Grid>
+        </article>
+      </section>
+    </div>
   );
 };
 
