3d3601537d30428169dcc41562df8b74292e3cba
Ron <ron@orbs.com>
2019-03-12 16:12:04 +0200

    register guardians and validators verifies ether distribution

---
+++ voting/test/it/truffle-scripts/helpers.js
@@ -0,0 +1,13 @@
+
+module.exports.verifyBalance = async function (web3, targetAccount, minBalance, bankAccount) {
+    const initial = await web3.eth.getBalance(targetAccount);
+    if (web3.utils.toBN(initial).gte(web3.utils.toBN(minBalance))) {
+        console.error(`verified balance for ${targetAccount} is at least ${minBalance} (${initial})`);
+        return;
+    }
+
+    const diff = web3.utils.toBN(minBalance).sub(web3.utils.toBN(initial)).toString();
+    console.error(`insufficient balance for ${targetAccount} transferring: ${diff}...`);
+    await web3.eth.sendTransaction({to:targetAccount, from:bankAccount, value:diff});
+    console.error(`new balance: ${await web3.eth.getBalance(targetAccount)}`);
+};
