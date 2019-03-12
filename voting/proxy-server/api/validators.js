a151b2958636868ce3413b0018f4fbf229495025
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-03-12 14:34:57 +0200

    proxy-server: intergrate with web3 and infura

--- voting/proxy-server/api/validators.js
+++ voting/proxy-server/api/validators.js
@@ -1,14 +1,38 @@
 const express = require('express');
+const OrbsContractsInfo = require('../contracts-info');
+const OrbsValidatorsContractJSON = require('../contracts/OrbsValidators.json');
+const OrbsValidatorsRegistryContractJSON = require('../contracts/OrbsValidatorsRegistry.json');
 
 const validatorsApiFactory = web3 => {
   const router = express.Router();
 
-  router.get('/validators', (req, res) => {
-    res.json(['3', '4']);
+  const validatorsContract = new web3.eth.Contract(
+    OrbsValidatorsContractJSON.abi,
+    OrbsContractsInfo.OrbsValidators.address
+  );
+
+  const validatorsRegistryContract = new web3.eth.Contract(
+    OrbsValidatorsRegistryContractJSON.abi,
+    OrbsContractsInfo.OrbsValidatorsRegistry.address
+  );
+
+  router.get('/validators', async (req, res) => {
+    const validators = await validatorsContract.methods
+      .getValidators()
+      .call();
+    res.json(validators);
   });
 
-  router.get('/validators/:address', (req, res) => {
-    res.json({ name: 'test', website: 'https://test.com' });
+  router.get('/validators/:address', async (req, res) => {
+    try {
+      const data = await validatorsRegistryContract.methods
+        .getValidatorData(req.params['address'])
+        .call();
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err.toString());
+    }
   });
 
   return router;
