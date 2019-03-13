a77dc393db74bb4cf35bec77a88cf8fdb0ad9217
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-03-13 15:28:15 +0200

    proxy-server: integrate with orbs-client-sdk

--- voting/proxy-server/api/guardians.js
+++ voting/proxy-server/api/guardians.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const Orbs = require('orbs-client-sdk');
 const OrbsContractsInfo = require('../contracts-info');
 const OrbsGuardiansContractJSON = require('../contracts/OrbsGuardians.json');
 
-const guardiansApiFactory = web3 => {
+const guardiansApiFactory = (web3, orbsAccount, orbsClient) => {
   const router = express.Router();
 
   const guardiansContract = new web3.eth.Contract(
@@ -23,9 +24,22 @@ const guardiansApiFactory = web3 => {
 
   router.get('/guardians/:address', async (req, res) => {
     try {
+      const address = req.params['address'];
       const data = await guardiansContract.methods
-        .getGuardianData(req.params['address'])
+        .getGuardianData(address)
         .call();
+
+      const query = orbsClient.createQuery(
+        orbsAccount.publicKey,
+        'BenchmarkToken',
+        'getBalance',
+        [Orbs.argAddress(address.toLowerCase())]
+      );
+
+      const {outputArguments} = await orbsClient.sendQuery(query);
+
+      data['balance'] = outputArguments[0].value.toString();
+
       res.json(data);
     } catch (err) {
       res.status(500).send(err.toString());
