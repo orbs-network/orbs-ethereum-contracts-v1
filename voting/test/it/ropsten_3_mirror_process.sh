d185c83ed09e335182238b2cbd632dd7fd119206
Ron <ron@orbs.com>
2019-03-13 16:55:23 +0200

    timeouts

--- voting/test/it/ropsten_3_mirror_process.sh
+++ voting/test/it/ropsten_3_mirror_process.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-go test . -run TestMirrorAndProcessOnRopsten -v -count 1
+go test . -run TestMirrorAndProcessOnRopsten -v -count 1 -timeout 30m
 
--- voting/test/it/ropsten_4_reclaim_ether.sh
+++ voting/test/it/ropsten_4_reclaim_ether.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-go test . -run TestReclaimGuardianDepositsOnRopsten -v -count 1
+go test . -run TestReclaimGuardianDepositsOnRopsten -v -count 1 -timeout 30m
 
