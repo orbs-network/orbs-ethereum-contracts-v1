e771ac5fbceb0ee7d7397244c1459445aec5b88f
Shai Yallin <shai.yallin@gmail.com>
2019-03-13 12:05:38 +0200

    trying to get golang tests to run in CI

--- voting/orbs/test.sh
+++ voting/orbs/test.sh
@@ -3,4 +3,7 @@ mkdir -p _out
 mkdir -p _reports
 go get -u github.com/orbs-network/go-junit-report
 go test ./_OrbsVoting/. -v &> _out/test.out || true # so that we always go to the junit report step
-go-junit-report -set-exit-code < _out/test.out > _out/results.xml
+EXITCODE=$?
+go-junit-report < _out/test.out > _out/results.xml
+cat _out/test.out
+exit $EXITCODE
