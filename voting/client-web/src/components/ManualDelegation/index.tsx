f6e0b9c29ade401dbc5b1ccdfd2374f9852dcd6d
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-03-17 15:48:50 +0200

    client-web: enable manual delegation

---
+++ voting/client-web/src/components/ManualDelegation/index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import { withStyles } from '@material-ui/core/styles';
+import DialogContent from '@material-ui/core/DialogContent';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
+const styles = () => ({});
+
+const DelegateButton = ({ onDelegate }) => {
+  return (
+    <Button
+      data-testid={`delegate-button`}
+      onClick={onDelegate}
+      variant="outlined"
+      color="secondary"
+    >
+      Delegate
+    </Button>
+  );
+};
+
+const ManualDelegationDialog = ({ dialogState, onClose, onDelegate }) => {
+  const [delegatorAddress, setDelegatorAddress] = useState('');
+
+  return (
+    <Dialog open={dialogState} onClose={onClose} maxWidth="lg" fullWidth={true}>
+      <DialogTitle>Manualy Delegate Your Stake</DialogTitle>
+      <DialogContent data-testid="manual-delegation-dialog">
+        <Typography variant="h6" color="textPrimary">
+          You can delegate your stake to anyone. They will need to delegate
+          their own stake & your stake to a guardian. Only stake delegated to a
+          voting guardian will be rewarded.
+        </Typography>
+        <TextField
+          required
+          data-testid="delegate-address-field"
+          placeholder="Delegate Ethereum Address"
+          value={delegatorAddress}
+          onChange={ev => setDelegatorAddress(ev.target.value)}
+          margin="normal"
+          variant="standard"
+          style={{ width: '40%' }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <DelegateButton onDelegate={() => onDelegate(delegatorAddress)} />
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default withStyles(styles)(ManualDelegationDialog);
