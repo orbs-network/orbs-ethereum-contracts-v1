361e4684feec9d11d5c1b53f0b73e240d42c3bd8
Shai Yallin <shai.yallin@gmail.com>
2019-03-18 13:54:18 +0200

    added dockerized test

--- subscription/ethereum/deploy.sh
+++
@@ -1,6 +0,0 @@
-./node_modules/.bin/truffle compile
-
-mkdir -p ../build/ethereum
-rm -rf ../build/ethereum/*
-
-node ../../stripper.js  ../build/ethereum/ build/contracts/*.json
---
+++ subscription/test/truffle-config.docker.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = process.env.ROPSTEN_SECRET;
+
+module.exports = {
+  networks: {
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, process.env.ROPSTEN_INFURA_URL, 0, 10),
+      network_id: '3',
+    },
+    ganache: {
+      host: 'ganache',
+      port: 7545,
+      network_id: '5777',
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.5.3',
+    },
+  },
+};
