d07e9354eeb00652487a8d339a6b0823105a53e3
Ron <ron@orbs.com>
2019-03-18 15:21:04 +0200

    fix mock topology contract

--- voting/ethereum/contracts/MockOrbsNetwork.sol
+++ voting/ethereum/contracts/MockOrbsNetwork.sol
@@ -5,13 +5,24 @@ import "./IOrbsNetworkTopology.sol";
 
 contract MockOrbsNetwork is IOrbsNetworkTopology {
 
-    bytes20[] nodes = new bytes20[](0);
-    bytes4[] ips = new bytes4[](0);
+bytes20[] nodes = [
+    bytes20(hex"6e2cb55e4cbe97bf5b1e731d51cc2c285d83cbf9"),
+    bytes20(hex"D27E2E7398E2582F63D0800330010B3E58952FF6"),
+    bytes20(hex"A328846CD5B4979D68A8C58A9BDFEEE657B34DE7"),
+    bytes20(hex"54018092153DCDEA764F89D33B086C7114E11985")
+];
+
+bytes4[] ips = [
+    bytes4(hex"0DEA8F0F"),
+    bytes4(hex"0DEA924A"),
+    bytes4(hex"0DEA931B"),
+    bytes4(hex"344221F9")
+];
 
     function getNetworkTopology()
         public
         view
-        returns (bytes20[] memory nodeAddresses,bytes4[] memory ipAddresses)
+        returns (bytes20[] memory nodeAddresses, bytes4[] memory ipAddresses)
     {
         return (nodes, ips);
     }
