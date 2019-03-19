c7756095dd080a238d51d8613cf3a02f303da5b0
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-03-19 08:56:34 +0200

    client-web: rename contracts

--- voting/client-web/debug.js
+++ voting/client-web/debug.js
@@ -1,26 +1,26 @@
 const Web3 = require('web3');
-const OrbsContractsInfo = require('./src/contracts-info');
-const OrbsGuardiansContractJSON = require('./src/contracts/OrbsGuardians.json');
-const OrbsValidatorsContractJSON = require('./src/contracts/OrbsValidators.json');
-const OrbsValidatorsRegistryContractJSON = require('./src/contracts/OrbsValidatorsRegistry.json');
+const contractsInfo = require('./src/contracts-info');
+const EthereumGuardiansContractJSON = require('./src/contracts/OrbsGuardians.json');
+const EthereumValidatorsContractJSON = require('./src/contracts/OrbsValidators.json');
+const EthereumValidatorsRegistryContractJSON = require('./src/contracts/OrbsValidatorsRegistry.json');
 
 (async () => {
   const web3 = new Web3('http://localhost:7545');
   const from = '0x9D4dB91AaA3573A67Ff7604EAcfC73d03e2C9c7A';
 
   const guardiansContract = new web3.eth.Contract(
-    OrbsGuardiansContractJSON.abi,
-    OrbsContractsInfo.OrbsGuardians.address
+    EthereumGuardiansContractJSON.abi,
+    contractsInfo.EthereumGuardiansContract.address
   );
 
   const validatorsContract = new web3.eth.Contract(
-    OrbsValidatorsContractJSON.abi,
-    OrbsContractsInfo.OrbsValidators.address
+    EthereumValidatorsContractJSON.abi,
+    contractsInfo.EthereumValidatorsContract.address
   );
 
   const validatorsRegistryContract = new web3.eth.Contract(
-    OrbsValidatorsRegistryContractJSON.abi,
-    OrbsContractsInfo.OrbsValidatorsRegistry.address
+    EthereumValidatorsRegistryContractJSON.abi,
+    contractsInfo.EthereumValidatorsRegistryContract.address
   );
 
   const guardians = await guardiansContract
--- voting/client-web/src/contracts-info.js
+++ voting/client-web/src/contracts-info.js
@@ -1,8 +1,14 @@
 module.exports = {
-  OrbsGuardians: { address: '0x3eEAca6307B56Ff085229b2329BEE6c3c009061F' },
-  OrbsValidators: { address: '0xe96E5b2798C9fC2FEF1FB92E4d44Cc8c491194CF' },
-  OrbsValidatorsRegistry: {
+  EthereumGuardiansContract: {
+    address: '0x3eEAca6307B56Ff085229b2329BEE6c3c009061F'
+  },
+  EthereumValidatorsContract: {
+    address: '0xe96E5b2798C9fC2FEF1FB92E4d44Cc8c491194CF'
+  },
+  EthereumValidatorsRegistryContract: {
     address: '0x7AC1Ff5469c3099012CF885e686280257dF2E0c0'
   },
-  OrbsVoting: { address: '0x1d230A72130E7a09204d3017d9BBF13552BDDe0b' }
+  EthereumVotingContract: {
+    address: '0x1d230A72130E7a09204d3017d9BBF13552BDDe0b'
+  }
 };
