5a19a7c21cacc09e0defc34d5311ef4d6f820ee6
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-03-21 12:57:11 +0200

    update addresses

--- voting/proxy-server/contracts-info.js
+++ voting/proxy-server/contracts-info.js
@@ -1,17 +1,17 @@
 module.exports = {
   EthereumGuardiansContract: {
-    address: '0x21fAE2f1835D7B90b74A303d0b4403feDe86d3ad'
+    address: '0x922864534F31153E182172e1a9cbf55064a19fc2'
   },
   EthereumValidatorsContract: {
-    address: '0x261d26232AFda7Bc7503E5531F9fD9002d626BC7'
+    address: '0xba6D4dfb4C2a9C68473F00D533e4A203BF133d49'
   },
   EthereumValidatorsRegistryContract: {
-    address: '0x9cd28Faf3Fe838e3EFE8ABc91c10fEBB1eA905F0'
+    address: '0x56FeA58d6Af7571ded986003C592FF9558b6FEB6'
   },
   EthereumVotingContract: {
-    address: '0x0d23a0624E0941696DC55afb8782c4Da162C83aC'
+    address: '0x3d08B99d1fB18A18Dd303C46fAB064c394f96f1c'
   },
   OrbsVotingContract: {
-    name: 'OrbsVoting_1553087430'
+    name: 'OrbsVoting_1553162341'
   }
 };
