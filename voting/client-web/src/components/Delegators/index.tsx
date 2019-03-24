38fc2e326ae7ac82a4746e2261dd8e61be475fa7
Tal Kol <talkol@gmail.com>
2019-03-24 13:46:56 +0200

    Added license files

--- voting/client-web/src/components/Delegators/index.tsx
+++ voting/client-web/src/components/Delegators/index.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright 2019 the orbs-ethereum-contracts authors
+ * This file is part of the orbs-ethereum-contracts library in the Orbs project.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+ * The above notice should be included in all copies or substantial portions of the software.
+ */
+
 import React, { useState, useEffect } from 'react';
 import GuardiansList from './list';
 import Link from '@material-ui/core/Link';
