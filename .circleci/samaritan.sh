b5370374ffbd2124e27dac85b7121d5987ed84f9
itamar Arjuan <itamar.arjuan@gmail.com>
2019-03-26 10:21:45 +0200

    adding vchain 1 instead of 0 and updating the block on ethereum to check per Oded

--- .circleci/samaritan.sh
+++ .circleci/samaritan.sh
@@ -4,12 +4,13 @@ export NETWORK_URL_ON_ETHEREUM="http://ec2-13-234-120-239.ap-south-1.compute.ama
 export ORBS_VOTING_CONTRACT_NAME="_Elections"
 export ERC20_CONTRACT_ADDRESS="0xeD0Aa9A4F9e5ae9092994f4B86F6AAa89944939b" # Ropsten
 export VOTING_CONTRACT_ADDRESS="0xF90a738CA659Fe99E357cB7F47Aaa5cB9b5724a2" #Ropsten
-export START_BLOCK_ON_ETHEREUM=500000 # Oded
+export START_BLOCK_ON_ETHEREUM=5275689 # Oded
 export END_BLOCK_ON_ETHEREUM=latest
-export ORBS_ENVIRONMENT=andrey
+export ORBS_ENVIRONMENT="andrey"
 export VERBOSE=1
 
 cd voting/processor
+npm install
 echo "**************** MIRRORING JOB STARTING **************"
 node mirror.js
 echo "**************** MIRRORING JOB FINISHED **************"
