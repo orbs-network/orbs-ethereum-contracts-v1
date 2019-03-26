df18b8cbd00356c9ed42430643a2093c4a499bcd
Ron <ron@orbs.com>
2019-03-26 15:44:44 +0200

    fixed deploy script to deploy testing ERC20 contract and link it to OrbsSubscriptions

--- subscription/ethereum/migrations/2_deploy_contracts.js
+++ subscription/ethereum/migrations/2_deploy_contracts.js
@@ -1,13 +1,18 @@
 const DateTime = artifacts.require('./DateTime.sol');
 const OrbsSubscriptions = artifacts.require('OrbsSubscriptions');
+const ERC20 = artifacts.require('./TestingERC20.sol');
 
 module.exports = async (deployer, network) => {
   // We're only using these migrations during development and testing.
-  if (network !== 'development' && network !== 'coverage') { // TODO - do we need this?
+  if (network !== 'development' && network !== 'coverage' && network !== 'ropsten') { // TODO - do we need this?
     return;
   }
 
+   await deployer.deploy(ERC20);
+
    await deployer.deploy(DateTime);
-   await deployer.link(DateTime, OrbsSubscriptions)
+   await deployer.link(DateTime, OrbsSubscriptions);
+
+   await deployer.deploy(OrbsSubscriptions, ERC20.address, ERC20.address, 1);
 };
 
