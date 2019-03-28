3ed7f84d834c3609b0d2e05214b109e65b58e45e
Ron <ron@orbs.com>
2019-03-28 16:47:06 +0200

    rename TestingERC20 project to orbs-solidity-mocks and use npm symlinks to import

--- stripper.sh
+++ stripper.sh
@@ -4,4 +4,4 @@ node stripper.js ./build/contracts/ $1
 
 # uncomment if you are doing this manully
 #node stripper.js ./build/contracts/ ./Federation/build/contracts/*.json
-#node stripper.js ./build/contracts/ ./TestingERC20/build/contracts/TestingERC20.json
+#node stripper.js ./build/contracts/ ./orbs-solidity-mocks/build/contracts/TestingERC20.json
