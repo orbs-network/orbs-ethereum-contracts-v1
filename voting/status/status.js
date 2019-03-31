512ebcdb4534da2447f94f3cda1e6f0746313e5a
Noam Berg <noamberg@yahoo.com>
2019-03-31 16:43:23 +0300

    eth network not part of project

--- voting/status/status.js
+++ voting/status/status.js
@@ -9,7 +9,7 @@ const Web3 = require('web3');
 const fs = require('fs');
 const _ = require('lodash/core');
 
-let ethereumConnectionURL = process.env.ETHEREUM_NETWORK_URL_ON_ETHEREUM;
+let ethereumConnectionURL = process.env.ETHEREUM_NETWORK_URL;
 let erc20ContractAddress = process.env.ERC20_CONTRACT_ADDRESS;
 let votingContractAddress = process.env.VOTING_CONTRACT_ADDRESS;
 let startBlock = process.env.START_BLOCK_ON_ETHEREUM;
@@ -25,7 +25,7 @@ function validateInput() {
     }
 
     if (!ethereumConnectionURL) {
-        throw("missing env variable ETHEREUM_NETWORK_URL_ON_ETHEREUM");
+        throw("missing env variable ETHEREUM_NETWORK_URL");
     }
 
     if (!erc20ContractAddress) {
