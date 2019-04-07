dddcf52c698eb8500aa53bc4b749c314d494e6e6
itamar Arjuan <itamar.arjuan@gmail.com>
2019-04-07 12:32:52 +0300

    making sure the samaritan runs with up to date values for production for the elections tomorrow

--- .circleci/samaritan.sh
+++ .circleci/samaritan.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-export NETWORK_URL_ON_ETHEREUM="http://ec2-13-234-120-239.ap-south-1.compute.amazonaws.com:8545"
-export ORBS_VOTING_CONTRACT_NAME="_Elections"
-export ERC20_CONTRACT_ADDRESS="0xeD0Aa9A4F9e5ae9092994f4B86F6AAa89944939b" # Ropsten
-export VOTING_CONTRACT_ADDRESS="0xF90a738CA659Fe99E357cB7F47Aaa5cB9b5724a2" #Ropsten
-export START_BLOCK_ON_ETHEREUM=5270000
-export END_BLOCK_ON_ETHEREUM=latest
-export ORBS_ENVIRONMENT="ropstentest"
+# I leave these as comments here for clarification on what is being passed through CircleCI
+#export NETWORK_URL_ON_ETHEREUM="http://ec2-13-234-120-239.ap-south-1.compute.amazonaws.com:8545"
+#export ORBS_VOTING_CONTRACT_NAME="_Elections"
+#export ERC20_CONTRACT_ADDRESS="0xeD0Aa9A4F9e5ae9092994f4B86F6AAa89944939b"
+#export VOTING_CONTRACT_ADDRESS="0xF90a738CA659Fe99E357cB7F47Aaa5cB9b5724a2"
+#export START_BLOCK_ON_ETHEREUM=5270000
+export END_BLOCK_ON_ETHEREUM="latest"
+#export ORBS_ENVIRONMENT="ropstentest"
 export VERBOSE=1
 
 cd voting/processor
