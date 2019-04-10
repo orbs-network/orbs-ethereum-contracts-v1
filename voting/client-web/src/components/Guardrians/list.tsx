eacdd2164f979db5efd1b901501d61e3bd42f908
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-04-10 15:22:56 +0300

    client-web: improve design of validators list

--- voting/client-web/src/components/Guardrians/list.tsx
+++ voting/client-web/src/components/Guardrians/list.tsx
@@ -6,7 +6,7 @@
  * The above notice should be included in all copies or substantial portions of the software.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '@material-ui/core/Link';
 import Table from '@material-ui/core/Table';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -15,8 +15,45 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import { withStyles } from '@material-ui/core/styles';
+import { Tooltip, SvgIcon, IconButton } from '@material-ui/core';
 
-const styles = () => ({});
+const styles = () => ({
+  table: {
+    tableLayout: 'fixed' as any
+  },
+  cell: {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis'
+  }
+});
+
+const CopyIcon = () => {
+  return (
+    <SvgIcon fontSize="small">
+      <path fill="none" d="M0 0h24v24H0V0z" />
+      <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4H8c-1.1 0-1.99.9-1.99 2L6 21c0 1.1.89 2 1.99 2H19c1.1 0 2-.9 2-2V11l-6-6zM8 21V7h6v5h5v9H8z" />
+    </SvgIcon>
+  );
+};
+
+const CopyAddress = ({ address }) => {
+  const [tooltipShown, setTooltopShown] = useState(false);
+  const copy = () => {
+    (navigator as any).clipboard.writeText(address).then(() => {
+      setTooltopShown(true);
+      setTimeout(() => {
+        setTooltopShown(false);
+      }, 1000);
+    });
+  };
+  return (
+    <Tooltip placement="top" title="Copied!" open={tooltipShown}>
+      <IconButton onClick={copy}>
+        <CopyIcon />
+      </IconButton>
+    </Tooltip>
+  );
+};
 
 const ValidatorsList = ({
   disableAll,
@@ -29,18 +66,33 @@ const ValidatorsList = ({
     <Table className={classes.table}>
       <TableHead>
         <TableRow>
-          <TableCell padding="checkbox" />
-          <TableCell>Name</TableCell>
-          <TableCell>Ethereum Address</TableCell>
-          <TableCell>Orbs Address</TableCell>
-          <TableCell>Website</TableCell>
-          <TableCell>Last election votes against (%)</TableCell>
+          <TableCell
+            style={{ width: '5%' }}
+            className={classes.cell}
+            padding="checkbox"
+          />
+          <TableCell style={{ width: '20%' }} className={classes.cell}>
+            Name
+          </TableCell>
+          <TableCell style={{ width: '4%' }} className={classes.cell} />
+          <TableCell style={{ width: '25%' }} className={classes.cell}>
+            Ethereum Address
+          </TableCell>
+          <TableCell style={{ width: '25%' }} className={classes.cell}>
+            Orbs Address
+          </TableCell>
+          <TableCell style={{ width: '20%' }} className={classes.cell}>
+            Website
+          </TableCell>
+          <TableCell style={{ width: '10%' }} className={classes.cell}>
+            Last election votes against (%)
+          </TableCell>
         </TableRow>
       </TableHead>
       <TableBody data-testid="validators-list">
         {Object.keys(validators).map(address => (
           <TableRow data-testid={`validator-${address}`} key={address}>
-            <TableCell padding="checkbox">
+            <TableCell className={classes.cell} padding="checkbox">
               {!readOnly && (
                 <Checkbox
                   disabled={!validators[address].checked && disableAll}
@@ -51,19 +103,39 @@ const ValidatorsList = ({
               )}
             </TableCell>
             <TableCell
+              className={classes.cell}
               component="th"
               scope="row"
               data-testid={`validator-${address}-name`}
             >
               {validators[address].name}
             </TableCell>
-            <TableCell data-testid={`validator-${address}-address`}>
-              {address}
-            </TableCell>
-            <TableCell data-testid={`validator-${address}-orbsAddress`}>
-              {validators[address].orbsAddress}
-            </TableCell>
             <TableCell>
+              {!!(navigator as any).clipboard && (
+                <CopyAddress address={address} />
+              )}
+            </TableCell>
+            <TableCell
+              className={classes.cell}
+              data-testid={`validator-${address}-address`}
+            >
+              <Tooltip title={address} placement="top-start" enterDelay={200}>
+                <span>{address}</span>
+              </Tooltip>
+            </TableCell>
+            <TableCell
+              className={classes.cell}
+              data-testid={`validator-${address}-orbsAddress`}
+            >
+              <Tooltip
+                title={validators[address].orbsAddress}
+                placement="top-start"
+                enterDelay={200}
+              >
+                <span>{validators[address].orbsAddress}</span>
+              </Tooltip>
+            </TableCell>
+            <TableCell className={classes.cell}>
               <Link
                 data-testid={`validator-${address}-url`}
                 href={validators[address].url}
@@ -75,7 +147,9 @@ const ValidatorsList = ({
                 {validators[address].url}
               </Link>
             </TableCell>
-            <TableCell>{validators[address].votesAgainst}</TableCell>
+            <TableCell className={classes.cell}>
+              {validators[address].votesAgainst}
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
