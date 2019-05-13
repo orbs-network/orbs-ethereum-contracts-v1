eb2579f294b6d4f68a8e5e19fc5fb30edf2c5c91
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-05-13 17:00:13 +0300

    feat(proxy-server): add delegation info endpoint

--- voting/proxy-server/api/delegation.js
+++ voting/proxy-server/api/delegation.js
@@ -7,6 +7,7 @@
  */
 
 const express = require('express');
+const { NON_DELEGATED } = require('../services/ethereum-client');
 
 const delegationApiFactory = ethereumClient => {
   const router = express.Router();
@@ -16,7 +17,7 @@ const delegationApiFactory = ethereumClient => {
    *
    * /delegation/status:
    *  get:
-   *    description: Returns the address of current delegator
+   *    description: Returns the address of current delegate
    *    tags:
    *      - Delegation
    *    parameters:
@@ -26,15 +27,76 @@ const delegationApiFactory = ethereumClient => {
    *        description: The address for whom the delegation status should be checked
    *    responses:
    *      '200':
-   *        description: Delegator address
+   *        description: Delegate address
    */
   router.get('/delegation/status', async (req, res) => {
     try {
-      const delegatedTo = await ethereumClient.getCurrentDelegation(
-        req.query['address']
-      );
-      res.send(delegatedTo);
+      const address = req.query['address'];
+      let info;
+      info = await ethereumClient.getCurrentDelegationByDelegate(address);
+      if (info.delegatedTo === NON_DELEGATED) {
+        info = await ethereumClient.getCurrentDelegationByTransfer(address);
+      }
+      res.send(info.delegatedTo);
     } catch (err) {
+      res.status(500).send(err.toString());
+    }
+  });
+
+  /**
+   * @swagger
+   *
+   * /delegation:
+   *  get:
+   *    description: Returns the information about the delegation
+   *    tags:
+   *      - Delegation
+   *    parameters:
+   *      - name: address
+   *        in: query
+   *        required: true
+   *        description: The address for whom the delegation status should be checked
+   *    responses:
+   *      '200':
+   *        description: Delegation information
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                delegatedTo:
+   *                  type: string
+   *                  description: The address of a delegate
+   *                delegatorBalance:
+   *                  type: string
+   *                  description: The amount of ORBS tokens that delegator posses
+   *                delegationType:
+   *                  type: string
+   *                  enum: [Delegate, Transfer]
+   *                  description: The method of delegation
+   *                delegationBlockNumber:
+   *                  type: string
+   *                  description: The block height when delegation has occurred
+   *                delegationTimestamp:
+   *                  type: string
+   *                  description: Time and date of delegation
+   */
+  router.get('/delegation', async (req, res) => {
+    try {
+      const address = req.query['address'];
+      let info;
+      info = await ethereumClient.getCurrentDelegationByDelegate(address);
+      if (info.delegatedTo === NON_DELEGATED) {
+        info = await ethereumClient.getCurrentDelegationByTransfer(address);
+        info.delegationType = 'Transfer';
+      } else {
+        info.delegationType = 'Delegate';
+      }
+      const balance = await ethereumClient.getOrbsBalance(address);
+      info.delegatorBalance = new Intl.NumberFormat('en').format(balance);
+      res.send(info);
+    } catch (err) {
+      console.error(err);
       res.status(500).send(err.toString());
     }
   });
