c0074f6633cfffcd669f18f5939e303cc78aec6b
Shai Yallin <shai.yallin@gmail.com>
2019-06-17 08:09:32 +0300

    added abstraction for orbs contract

--- psilo/orbsAdapter.js
+++ psilo/orbsAdapter.js
@@ -11,6 +11,10 @@ class OrbsAdapter {
         this.client = new Orbs.Client(url, vChainId, Orbs.NetworkType.NETWORK_TYPE_TEST_NET)
     }
 
+    contract(name) {
+        return new OrbsContract(name, this.client);
+    }
+
     static async build() {
 
         // TODO - select network to run against.
@@ -18,6 +22,24 @@ class OrbsAdapter {
         const gammaUrl = process.env.GAMMA_URL || "http://localhost:8080";
         return new OrbsAdapter(gammaUrl, 42, 10, 2);
     }
+}
+
+class OrbsContract {
+    constructor(name, client) {
+        this.name = name;
+        this.client = client;
+    }
+
+    transact(signer, methodName, ...args) {
+        const [t] = this.client.createTransaction(signer.publicKey, signer.privateKey, this.name, methodName, args);
+        return this.client.sendTransaction(t);
+    }
+
+    query(signer, methodName, ...args) {
+        const q = this.client.createQuery(signer.publicKey, this.name, methodName, args);
+        return this.client.sendQuery(q);
+    }
+
 }
 
 module.exports = {OrbsAdapter};
