deb759caae7b5450e2833d7b5fbca3761f7cc696
Noam Berg <noamberg@yahoo.com>
2019-07-10 18:11:02 +0300

    remove gamma-cli from the process/mirror/isalive

--- .circleci/samaritan-processing.sh
+++ .circleci/samaritan-processing.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 # I leave these as comments here for clarification on what is being passed through CircleCI
-#export NETWORK_URL_ON_ETHEREUM="http://:8545"
+#export ORBS_URL="http://35.183.225.241/vchains/1100000"
+#export ORBS_URL=1100000
 #export ORBS_VOTING_CONTRACT_NAME="_Elections"
-#export ERC20_CONTRACT_ADDRESS="0xff56Cc6b1E6dEd347aA0B7676C85AB0B3D08B0FA"
-#export VOTING_CONTRACT_ADDRESS="0x30f855afb78758Aa4C2dc706fb0fA3A98c865d2d"
-#export START_BLOCK_ON_ETHEREUM=7439509
-export END_BLOCK_ON_ETHEREUM="latest"
-#export ORBS_ENVIRONMENT="production"
+#export MAXIMUM_NUMBER_OF_TRIES=2500
+#export BATCH_SIZE=10
 export VERBOSE=1
 
 cd voting/processor
