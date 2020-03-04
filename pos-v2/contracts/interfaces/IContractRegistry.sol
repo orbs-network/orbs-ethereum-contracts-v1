ee62d4c88d4cdd23bc94492369da6fc0aaaf3769
OdedWx <odedw@orbs.com>
2020-03-04 15:29:22 +0200

    interfaces review

--- pos-v2/contracts/interfaces/IContractRegistry.sol
+++ pos-v2/contracts/interfaces/IContractRegistry.sol
@@ -1,10 +1,11 @@
 pragma solidity 0.5.16;
 
 interface IContractRegistry {
+	event ContractSet(string contractName, address addr);
 
 	/// @dev updates the contracts address and emits a corresponding event
-	function set(string calldata contractName, address addr) external /* onlyGovernor */;
+	function setContractAddress(string calldata contractName, address addr) external /* onlyGovernor */;
 
 	/// @dev returns the current address of the
-	function get(string calldata contractName) external view returns (address);
+	function getContractAddress(string calldata contractName) external view returns (address);
 }
