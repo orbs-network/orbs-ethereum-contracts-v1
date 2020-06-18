22f54469f8ccf8272415007bd125b62f3b368595
Ron <ron@orbs.com>
2020-06-18 14:10:52 +0300

    remove useless functions in IStakingContract

--- rewards-distribution/ethereum/contracts/IStakingContract.sol
+++ rewards-distribution/ethereum/contracts/IStakingContract.sol
@@ -1,25 +1,8 @@
 pragma solidity 0.4.26;
 
 
-/// @title An interface for staking contracts.
+/// @title An interface for distributing rewards to staking contracts.
 interface IStakingContract {
-    /// @dev Stakes ORBS tokens on behalf of msg.sender. This method assumes that the user has already approved at least
-    /// the required amount using ERC20 approve.
-    /// @param _amount uint256 The amount of tokens to stake.
-    function stake(uint256 _amount) external;
-
-    /// @dev Unstakes ORBS tokens from msg.sender. If successful, this will start the cooldown period, after which
-    /// msg.sender would be able to withdraw all of his tokens.
-    /// @param _amount uint256 The amount of tokens to unstake.
-    function unstake(uint256 _amount) external;
-
-    /// @dev Requests to withdraw all of staked ORBS tokens back to msg.sender. Stake owners can withdraw their ORBS
-    /// tokens only after previously unstaking them and after the cooldown period has passed (unless the contract was
-    /// requested to release all stakes).
-    function withdraw() external;
-
-    /// @dev Restakes unstaked ORBS tokens (in or after cooldown) for msg.sender.
-    function restake() external;
 
     /// @dev Distributes staking rewards to a list of addresses by directly adding rewards to their stakes. This method
     /// assumes that the user has already approved at least the required amount using ERC20 approve. Since this is a
@@ -29,26 +12,4 @@ interface IStakingContract {
     /// @param _stakeOwners address[] The addresses of the stake owners.
     /// @param _amounts uint256[] The amounts of the rewards.
     function distributeRewards(uint256 _totalAmount, address[] _stakeOwners, uint256[] _amounts) external;
-
-    /// @dev Returns the stake of the specified stake owner (excluding unstaked tokens).
-    /// @param _stakeOwner address The address to check.
-    /// @return uint256 The total stake.
-    function getStakeBalanceOf(address _stakeOwner) external view returns (uint256);
-
-    /// @dev Returns the total amount staked tokens (excluding unstaked tokens).
-    /// @return uint256 The total staked tokens of all stake owners.
-    function getTotalStakedTokens() external view returns (uint256);
-
-    /// @dev Returns the time that the cooldown period ends (or ended) and the amount of tokens to be released.
-    /// @param _stakeOwner address The address to check.
-    /// @return cooldownAmount uint256 The total tokens in cooldown.
-    /// @return cooldownEndTime uint256 The time when the cooldown period ends (in seconds).
-    function getUnstakeStatus(address _stakeOwner) external view returns (uint256 cooldownAmount,
-        uint256 cooldownEndTime);
-
-    event Staked(address indexed stakeOwner, uint256 amount, uint256 totalStakedAmount);
-    event Unstaked(address indexed stakeOwner, uint256 amount, uint256 totalStakedAmount);
-    event Withdrew(address indexed stakeOwner, uint256 amount, uint256 totalStakedAmount);
-    event Restaked(address indexed stakeOwner, uint256 amount, uint256 totalStakedAmount);
-    event MigratedStake(address indexed stakeOwner, uint256 amount, uint256 totalStakedAmount);
 }
