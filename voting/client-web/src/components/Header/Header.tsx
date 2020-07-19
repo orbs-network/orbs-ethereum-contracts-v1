93811191b5ce68963d68c451320799b0375530f6
Or Lavy <orlavyil@gmail.com>
2020-07-19 16:44:44 +0300

    Addsd working popover menu for mobile

--- voting/client-web/src/components/Header/Header.tsx
+++ voting/client-web/src/components/Header/Header.tsx
@@ -8,7 +8,7 @@
 
 import AppBar from '@material-ui/core/AppBar';
 import Link from '@material-ui/core/Link';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme, withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import classNames from 'classnames';
 import React, { useCallback, useMemo, useState } from 'react';
@@ -17,9 +17,11 @@ import { NavLink } from 'react-router-dom';
 import { ReadOnlyBanner } from '../ReadOnlyBanner/ReadOnlyBanner';
 import { Languages } from './languages';
 import logo from './logo-white.svg';
-import { Button } from '@material-ui/core';
+import { Button, IconButton, useMediaQuery } from '@material-ui/core';
 import { useApi } from '../../services/ApiContext';
 import { HEADER_HEIGHT_REM } from '../App/ThemeProvider';
+import { MenuPopup } from './MenuPopup';
+import { useLinkDescriptors } from './links';
 
 export const HOVER_COLOR = '#16faff';
 
@@ -83,13 +85,38 @@ export const Header = React.memo(props => {
   const [isNoMetamaskBannerOpen, setIsMetamaskBannerOpen] = useState(!hasMetamask);
   const hideMetaMaskBanner = useCallback(() => setIsMetamaskBannerOpen(false), [setIsMetamaskBannerOpen]);
 
-  const links = [
-    { label: t('Home'), url: '/' },
-    { label: t('Guardians'), url: '/delegator' },
-    { label: t('Validators'), url: '/guardian' },
-    { label: t('Elected Validators'), url: '/validator' },
-    { label: t('Rewards'), url: '/reward' },
-  ];
+  const theme = useTheme();
+  const smallerThanSmall = useMediaQuery(theme.breakpoints.down('xs'));
+  const linkDescriptors = useLinkDescriptors();
+
+  // C.F.H : add proper menu for smaller screens
+
+  const menuLinks = useMemo(() => {
+    return linkDescriptors.map(({ label, url }, idx) => (
+      <Link
+        // @ts-ignore
+        component={NavLink}
+        key={idx}
+        exact={true}
+        className={classes.link}
+        activeStyle={{ color: HOVER_COLOR }}
+        underline='none'
+        to={url}
+        variant='h6'
+        noWrap
+      >
+        {label}
+      </Link>
+    ));
+  }, [classes.link, linkDescriptors]);
+
+  const menu = useMemo(() => {
+    if (smallerThanSmall) {
+      return <MenuPopup />;
+    } else {
+      return <nav className={classes.nav}>{menuLinks}</nav>;
+    }
+  }, [classes.nav, menuLinks, smallerThanSmall]);
 
   return (
     <AppBar
@@ -104,7 +131,7 @@ export const Header = React.memo(props => {
       <ReadOnlyBanner isOpen={isNoMetamaskBannerOpen} closeBanner={hideMetaMaskBanner} />
 
       <div className={classes.headerButtonsContainer}>
-        {metamask && (
+        {metamask && !smallerThanSmall && (
           <Button
             size='small'
             variant='outlined'
@@ -121,24 +148,7 @@ export const Header = React.memo(props => {
         <NavLink to='/'>
           <img className={classes.logo} src={logo} alt='Orbs' />
         </NavLink>
-        <nav className={classes.nav}>
-          {links.map(({ label, url }, idx) => (
-            <Link
-              // @ts-ignore
-              component={NavLink}
-              key={idx}
-              exact={true}
-              className={classes.link}
-              activeStyle={{ color: HOVER_COLOR }}
-              underline='none'
-              to={url}
-              variant='h6'
-              noWrap
-            >
-              {label}
-            </Link>
-          ))}
-        </nav>
+        {menu}
       </Toolbar>
     </AppBar>
   );
